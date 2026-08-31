93af32b2119d8c41348a2a1208d7b73d1c243594
Morningstar202604 <Morningstar202604@users.noreply.github.com>
2026-09-01 04:09:24 +0800

    fix: Kotlin 版本从 2.3.30 改为 2.3.21（2.3.30 不存在，2.3.21 是 2.3.x 最新稳定版）

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,9 +1,9 @@
 plugins {
     id("com.android.application") version "8.13.2" apply false
-    id("org.jetbrains.kotlin.android") version "2.3.30" apply false
-    id("org.jetbrains.kotlin.jvm") version "2.3.30" apply false
-    id("org.jetbrains.kotlin.plugin.compose") version "2.3.30" apply false
-    id("org.jetbrains.kotlin.plugin.serialization") version "2.3.30" apply false
+    id("org.jetbrains.kotlin.android") version "2.3.21" apply false
+    id("org.jetbrains.kotlin.jvm") version "2.3.21" apply false
+    id("org.jetbrains.kotlin.plugin.compose") version "2.3.21" apply false
+    id("org.jetbrains.kotlin.plugin.serialization") version "2.3.21" apply false
     id("com.google.devtools.ksp") version "2.3.11" apply false
     id("com.google.dagger.hilt.android") version "2.52" apply false
 }
